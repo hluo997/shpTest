8c1b4e8cc2e530814fc370ce87c467f9d5078983
hluo997 <hluo997@gmail.com>
2016-04-29 15:39:02 -0700

    Test commit spring project

---
+++ shpTest/src/main/java/com/shp/test/ShpTestApplication.java
@@ -0,0 +1,12 @@
+package com.shp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShpTestApplication.class, args);
+	}
+}
